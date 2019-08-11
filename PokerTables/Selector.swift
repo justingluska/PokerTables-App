d5d5fdb47161749d01282c55170e80e784c172e3
Justin Gluska <justingluska@gmail.com>
2019-08-11 00:30:38 -0400

    Removed pics for size

--- PokerTables/Selector.swift
+++ PokerTables/Selector.swift
@@ -11,9 +11,16 @@ import UIKit
 class Selector: UIViewController {
 
     override func viewDidLoad() {
+        butt2.layer.cornerRadius = 7
+        butt3.layer.cornerRadius = 7
+        butt4.layer.cornerRadius = 7
         back.layer.cornerRadius = 15
         super.viewDidLoad()
     }
+    
+    @IBOutlet weak var butt2: UIButton!
+    @IBOutlet weak var butt3: UIButton!
+    @IBOutlet weak var butt4: UIButton!
     
     @IBOutlet weak var back: UIButton!
     
