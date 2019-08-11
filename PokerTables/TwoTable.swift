0f57c7331b1286ef7f0ac6b264466c70abe7ca52
Justin Gluska <justingluska@gmail.com>
2019-08-11 18:25:25 -0400

    TwoTable fully working

--- PokerTables/TwoTable.swift
+++ PokerTables/TwoTable.swift
@@ -13,6 +13,12 @@ class TwoTable: UIViewController, UITextFieldDelegate {
         backB.layer.cornerRadius = 10
         clearB.layer.cornerRadius = 10
         generateB.layer.cornerRadius = 15
+        thirdStack.isHidden = true
+        fourthStack.isHidden = true
+        fifthStack.isHidden = true
+        sixthStack.isHidden = true
+        seventhStack.isHidden = true
+        eigthStack.isHidden = true
         self.p1Name.delegate = self
         self.p2Name.delegate = self
         self.p3Name.delegate = self
@@ -31,6 +37,15 @@ class TwoTable: UIViewController, UITextFieldDelegate {
         self.p16Name.delegate = self
         super.viewDidLoad()
     }
+    
+    @IBOutlet weak var firstStack: UIStackView!
+    @IBOutlet weak var secondStack: UIStackView!
+    @IBOutlet weak var thirdStack: UIStackView!
+    @IBOutlet weak var fourthStack: UIStackView!
+    @IBOutlet weak var fifthStack: UIStackView!
+    @IBOutlet weak var sixthStack: UIStackView!
+    @IBOutlet weak var seventhStack: UIStackView!
+    @IBOutlet weak var eigthStack: UIStackView!
     
     @IBOutlet weak var backB: UIButton!
     @IBOutlet weak var clearB: UIButton!
@@ -304,130 +319,74 @@ class TwoTable: UIViewController, UITextFieldDelegate {
         twoPlayerAmount = Int(sender.value) * 2
         twoPlayerLabel.text = "\(twoPlayerAmount) players"
         if(twoPlayerAmount == 4){
-            p1Name.isHidden = false
-            p2Name.isHidden = false
-            p3Name.isHidden = false
-            p4Name.isHidden = false
-            p5Name.isHidden = true
-            p6Name.isHidden = true
-            p7Name.isHidden = true
-            p8Name.isHidden = true
-            p9Name.isHidden = true
-            p10Name.isHidden = true
-            p11Name.isHidden = true
-            p12Name.isHidden = true
-            p13Name.isHidden = true
-            p14Name.isHidden = true
-            p15Name.isHidden = true
-            p16Name.isHidden = true
+            firstStack.isHidden = false
+            secondStack.isHidden = false
+            thirdStack.isHidden = true
+            fourthStack.isHidden = true
+            fifthStack.isHidden = true
+            sixthStack.isHidden = true
+            seventhStack.isHidden = true
+            eigthStack.isHidden = true
         }
         else if(twoPlayerAmount == 6){
-            p1Name.isHidden = false
-            p2Name.isHidden = false
-            p3Name.isHidden = false
-            p4Name.isHidden = false
-            p5Name.isHidden = false
-            p6Name.isHidden = false
-            p7Name.isHidden = true
-            p8Name.isHidden = true
-            p9Name.isHidden = true
-            p10Name.isHidden = true
-            p11Name.isHidden = true
-            p12Name.isHidden = true
-            p13Name.isHidden = true
-            p14Name.isHidden = true
-            p15Name.isHidden = true
-            p16Name.isHidden = true
+            firstStack.isHidden = false
+            secondStack.isHidden = false
+            thirdStack.isHidden = false
+            fourthStack.isHidden = true
+            fifthStack.isHidden = true
+            sixthStack.isHidden = true
+            seventhStack.isHidden = true
+            eigthStack.isHidden = true
         }
         else if(twoPlayerAmount == 8){
-            p1Name.isHidden = false
-            p2Name.isHidden = false
-            p3Name.isHidden = false
-            p4Name.isHidden = false
-            p5Name.isHidden = false
-            p6Name.isHidden = false
-            p7Name.isHidden = false
-            p8Name.isHidden = false
-            p9Name.isHidden = true
-            p10Name.isHidden = true
-            p11Name.isHidden = true
-            p12Name.isHidden = true
-            p13Name.isHidden = true
-            p14Name.isHidden = true
-            p15Name.isHidden = true
-            p16Name.isHidden = true
+            firstStack.isHidden = false
+            secondStack.isHidden = false
+            thirdStack.isHidden = false
+            fourthStack.isHidden = false
+            fifthStack.isHidden = true
+            sixthStack.isHidden = true
+            seventhStack.isHidden = true
+            eigthStack.isHidden = true
         }
         else if(twoPlayerAmount == 10){
-            p1Name.isHidden = false
-            p2Name.isHidden = false
-            p3Name.isHidden = false
-            p4Name.isHidden = false
-            p5Name.isHidden = false
-            p6Name.isHidden = false
-            p7Name.isHidden = false
-            p8Name.isHidden = false
-            p9Name.isHidden = false
-            p10Name.isHidden = false
-            p11Name.isHidden = true
-            p12Name.isHidden = true
-            p13Name.isHidden = true
-            p14Name.isHidden = true
-            p15Name.isHidden = true
-            p16Name.isHidden = true
+            firstStack.isHidden = false
+            secondStack.isHidden = false
+            thirdStack.isHidden = false
+            fourthStack.isHidden = false
+            fifthStack.isHidden = false
+            sixthStack.isHidden = true
+            seventhStack.isHidden = true
+            eigthStack.isHidden = true
         }
         else if(twoPlayerAmount == 12){
-            p1Name.isHidden = false
-            p2Name.isHidden = false
-            p3Name.isHidden = false
-            p4Name.isHidden = false
-            p5Name.isHidden = false
-            p6Name.isHidden = false
-            p7Name.isHidden = false
-            p8Name.isHidden = false
-            p9Name.isHidden = false
-            p10Name.isHidden = false
-            p11Name.isHidden = false
-            p12Name.isHidden = false
-            p13Name.isHidden = true
-            p14Name.isHidden = true
-            p15Name.isHidden = true
-            p16Name.isHidden = true
+            firstStack.isHidden = false
+            secondStack.isHidden = false
+            thirdStack.isHidden = false
+            fourthStack.isHidden = false
+            fifthStack.isHidden = false
+            sixthStack.isHidden = false
+            seventhStack.isHidden = true
+            eigthStack.isHidden = true
         }
         else if(twoPlayerAmount == 14){
-            p1Name.isHidden = false
-            p2Name.isHidden = false
-            p3Name.isHidden = false
-            p4Name.isHidden = false
-            p5Name.isHidden = false
-            p6Name.isHidden = false
-            p7Name.isHidden = false
-            p8Name.isHidden = false
-            p9Name.isHidden = false
-            p10Name.isHidden = false
-            p11Name.isHidden = false
-            p12Name.isHidden = false
-            p13Name.isHidden = false
-            p14Name.isHidden = false
-            p15Name.isHidden = true
-            p16Name.isHidden = true
+            firstStack.isHidden = false
+            secondStack.isHidden = false
+            thirdStack.isHidden = false
+            fourthStack.isHidden = false
+            fifthStack.isHidden = false
+            sixthStack.isHidden = false
+            seventhStack.isHidden = false
+            eigthStack.isHidden = true
         }
         else if(twoPlayerAmount == 16){
-            p1Name.isHidden = false
-            p2Name.isHidden = false
-            p3Name.isHidden = false
-            p4Name.isHidden = false
-            p5Name.isHidden = false
-            p6Name.isHidden = false
-            p7Name.isHidden = false
-            p8Name.isHidden = false
-            p9Name.isHidden = false
-            p10Name.isHidden = false
-            p11Name.isHidden = false
-            p12Name.isHidden = false
-            p13Name.isHidden = false
-            p14Name.isHidden = false
-            p15Name.isHidden = false
-            p16Name.isHidden = false
+            firstStack.isHidden = false
+            secondStack.isHidden = false
+            thirdStack.isHidden = false
+            fourthStack.isHidden = false
+            fifthStack.isHidden = false
+            sixthStack.isHidden = false
+            seventhStack.isHidden = false
+            eigthStack.isHidden = false
         }
 
     }
